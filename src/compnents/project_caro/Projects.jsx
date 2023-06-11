9ffa09ec48d9a28d864d679d32455c1a62055578
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-06-11 22:01:59 +0530

    removed extra codes

--- src/compnents/project_caro/Projects.jsx
+++ src/compnents/project_caro/Projects.jsx
@@ -1,168 +1,66 @@
-import { GitHub } from '@mui/icons-material'
-import React, { useEffect, useRef, useState } from 'react'
-import './project.css'
-import {motion} from 'framer-motion'
+import { GitHub } from "@mui/icons-material";
+import React, { useEffect, useRef, useState } from "react";
+import "./project.css";
+import { motion } from "framer-motion";
+import { projects } from "../../lib/projects";
 
 export default function Projects() {
-
-    const[width, setWidth] = useState(0);
-    const carousel = useRef(0);
-    useEffect(()=>{
-        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-        console.log(carousel.current.scrollWidth);
-        console.log(carousel.current.offsetWidth);
-    },[carousel.current.scrollWidth]);
+  const [width, setWidth] = useState(0);
+  const carousel = useRef(0);
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    console.log(carousel.current.scrollWidth);
+    console.log(carousel.current.offsetWidth);
+  }, [carousel.current.offsetWidth]);
 
   return (
     //inner caro - project-carousel
     <>
-    <div id='project' className='project-container'> 
-        <div className='project-wrapper'> 
-        <motion.div ref={carousel} whileTap={{cursor:"grabbing"}} className='caro-wrapper'> 
-        <img className='project-hero' src='https://cdn.dribbble.com/users/528264/screenshots/3140440/firebase_logo.png'/>
-            <p className='project-heading'>Projects.</p>
-            <p className='project-des'>These are some of the projects I have worked on.</p>
-            <motion.div drag="x" dragConstraints={{right:0, left: -width}} className='project-carousel'>
-            <div className='project'>
-                    <img className='project-img' src='/music.png'/>
-                    <div className='project-overview'>
-                        <div className='about-project'>
-                            <div className='project-details'>
-                                <p className='project-title'>Music</p>
-                                <p className='project-category'>Mood based Music Recommendation</p>
-                            </div>
-                            <a className='github-linker' href='https://github.com/CoffeeGeek101/Music'>
-                            <GitHub/>
-                            </a>
-                        </div>
-                        <div className='project-stack'>
-                            <button className='project-stack-tag'>NextJS</button>
-                            <button className='project-stack-tag'>Redis</button>
-                            <button className='project-stack-tag'>TailwindCSS</button>
-                            <button className='project-stack-tag'>APIs</button>
-                        </div>
+      <div id="project" className="project-container">
+        <div className="project-wrapper">
+          <motion.div
+            ref={carousel}
+            whileTap={{ cursor: "grabbing" }}
+            className="caro-wrapper"
+          >
+            <img
+              className="project-hero"
+              src="https://cdn.dribbble.com/users/528264/screenshots/3140440/firebase_logo.png"
+            />
+            <p className="project-heading">Projects.</p>
+            <p className="project-des">
+              These are some of the projects I have worked on.
+            </p>
+            <motion.div
+              drag="x"
+              dragConstraints={{ right: 0, left: -width }}
+              className="project-carousel"
+            >
+              {projects.map((project) => (
+                <div className="project" key={project.link}>
+                  <img className="project-img" src={project.img} />
+                  <div className="project-overview">
+                    <div className="about-project">
+                      <div className="project-details">
+                        <p className="project-title">{project.title}</p>
+                        <p className="project-category">{project.des}</p>
+                      </div>
+                      <a className="github-linker" href={project.link}>
+                        <GitHub />
+                      </a>
                     </div>
-                </div>
-                <div className='project'>
-                    <img className='project-img' src='/movie.png'/>
-                    <div className='project-overview'>
-                        <div className='about-project'>
-                            <div className='project-details'>
-                                <p className='project-title'>MovieMate</p>
-                                <p className='project-category'>Latest and Trending Movie guide</p>
-                            </div>
-                            <a className='github-linker' href='https://github.com/CoffeeGeek101/MovieMate'>
-                            <GitHub/>
-                            </a>
-                        </div>
-                        <div className='project-stack'>
-                            <button className='project-stack-tag'>React</button>
-                            <button className='project-stack-tag'>ReduxSaga</button>
-                            <button className='project-stack-tag'>APIs</button>
-                        </div>
+                    <div className="project-stack">
+                      {project.stack.map((tech) => (
+                        <button className="project-stack-tag">{tech}</button>
+                      ))}
                     </div>
+                  </div>
                 </div>
-                <div className='project'>
-                    <img className='project-img' src='/chat-u.png'/>
-                    <div className='project-overview'>
-                        <div className='about-project'>
-                            <div className='project-details'>
-                                <p className='project-title'>chatU</p>
-                                <p className='project-category'>Realtime Chat Application</p>
-                            </div>
-                            <a className='github-linker' href='https://github.com/CoffeeGeek101/chatU'>
-                            <GitHub/>
-                            </a>
-                        </div>
-                        <div className='project-stack'>
-                            <button className='project-stack-tag'>React</button>
-                            <button className='project-stack-tag'>Firebase</button>
-                            <button className='project-stack-tag'>Framer</button>
-                            <button className='project-stack-tag'>APIs</button>
-                        </div>
-                    </div>
-                </div>
-                <div className='project'>
-                    <img className='project-img' src='/weather.png'/>
-                    <div className='project-overview'>
-                        <div className='about-project'>
-                            <div className='project-details'>
-                                <p className='project-title'>Weather Application</p>
-                                <p className='project-category'>Modern weather UI</p>
-                            </div>
-                            <a className='github-linker' href='https://github.com/CoffeeGeek101/Modern-weather-app.'>
-                            <GitHub/>
-                            </a>
-                        </div>
-                        <div className='project-stack'>
-                            <button className='project-stack-tag'>React</button>
-                            <button className='project-stack-tag'>AsyncPaginate</button>
-                            <button className='project-stack-tag'>APIs</button>
-                        </div>
-                    </div>
-                </div>
-                <div className='project'>
-                    <img className='project-img' src='/travel.png'/>
-                    <div className='project-overview'>
-                        <div className='about-project'>
-                            <div className='project-details'>
-                                <p className='project-title'>yourTravel</p>
-                                <p className='project-category'>Travel Agency Landing page</p>
-                            </div>
-                            <a className='github-linker' href='https://github.com/CoffeeGeek101/YourTravel'>
-                            <GitHub/>
-                            </a>
-                        </div>
-                        <div className='project-stack'>
-                            <button className='project-stack-tag'>React</button>
-                            <button className='project-stack-tag'>TailwindCSS</button>
-                            <button className='project-stack-tag'>Framer</button>
-                        </div>
-                    </div>
-                </div>
-                <div className='project'>
-                    <img className='project-img' src='https://w0.peakpx.com/wallpaper/172/343/HD-wallpaper-netflix-logo-black-logo-minimal-netflix.jpg'/>
-                    <div className='project-overview'>
-                        <div className='about-project'>
-                            <div className='project-details'>
-                                <p className='project-title'>Netflix Clone</p>
-                                <p className='project-category'>FullStack</p>
-                            </div>
-                            <a className='github-linker' href='https://github.com/CoffeeGeek101/Netflix-frontend'>
-                            <GitHub/>
-                            </a>
-                        </div>
-                        <div className='project-stack'>
-                            <button className='project-stack-tag'>React</button>
-                            <button className='project-stack-tag'>MonogDB</button>
-                            <button className='project-stack-tag'>Axios</button>
-                            <button className='project-stack-tag'>APIs</button>
-                        </div>
-                    </div>
-                </div>
-                <div className='project'>
-                    <img className='project-img' src='/port-folio.png'/>
-                    <div className='project-overview'>
-                        <div className='about-project'>
-                            <div className='project-details'>
-                                <p className='project-title'>Modern Portfolio UI</p>
-                                <p className='project-category'>Frontend UI</p>
-                            </div>
-                            <a className='github-linker' href='https://shoumyadeep.netlify.app'>
-                            <GitHub/>
-                            </a>
-                        </div>
-                        <div className='project-stack'>
-                            <button className='project-stack-tag'>React</button>
-                            <button className='project-stack-tag'>Framer</button>
-                            <button className='project-stack-tag'>CSS</button>
-                        </div>
-                    </div>
-                </div>
+              ))}
             </motion.div>
-        </motion.div>
+          </motion.div>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }

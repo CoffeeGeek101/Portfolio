86a6cd1c1122fe0a146b036f9a55f80b0bdcce50
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-06-11 22:00:32 +0530

    add : techstack & project Data

---
+++ src/lib/techstackData.js
@@ -0,0 +1,42 @@
+export const techstack = [
+    {
+        name : 'TypeScript',
+        img : 'https://cdn.hashnode.com/res/hashnode/image/upload/v1628508546375/5cUotnuhg.png',
+
+    },
+    {
+        name : 'Next',
+        img : 'https://images.ctfassets.net/23aumh6u8s0i/c04wENP3FnbevwdWzrePs/1e2739fa6d0aa5192cf89599e009da4e/nextjs',
+        
+    },
+    {
+        name : 'React',
+        img : 'https://www.komododigital.co.uk/app/uploads/2021/05/React-1.jpg',
+        
+    },
+    {
+        name : 'HTML',
+        img : 'https://pixelmechanics.com.sg/wp-content/uploads/2019/06/html5-logo-for-web-development.png',
+        
+    },
+    {
+        name : 'Tailwind CSS',
+        img : 'https://www.devonblog.com/wp-content/uploads/2022/06/tailwind-thumb.jpg',
+        
+    },
+    {
+        name : 'Redis',
+        img : 'https://www.mortensi.com/wp-content/uploads/2021/10/Redis-server.jpg',
+        
+    },
+    {
+        name : 'GitHub',
+        img : 'https://i.pinimg.com/originals/b5/1b/78/b51b78ecc9e5711274931774e433b5e6.png',
+    
+    }, 
+    {
+        name : 'MongoDB',
+        img : 'https://pbs.twimg.com/profile_images/1452637606559326217/GFz_P-5e_400x400.png',
+    
+    }, 
+]
